ae3061a62f2f1c87f771b7b358ab12dc591bf8a0
IvanZuev <ivan78.ru@yandex.ru>
2020-11-01 22:20:36 +0300

    where is my pre-commit?

---
+++ HomeWork1/Tests/test_task05.py
@@ -0,0 +1,14 @@
+import unittest
+
+from HomeWork1.Tasks.task05 import find_maximal_subarray_sum
+
+class TestTask03(unittest.TestCase):
+    def setUp(self) -> None:
+        self.example_array = [1, 3, -1, -3, 5, 3, 6, 7]
+
+    def test_example(self):
+        self.assertEqual(find_maximal_subarray_sum([1, 3, -1, -3, 5, 3, 6, 7], 3), 16)
+
+
+if __name__ == '__main__':
+    unittest.main()
