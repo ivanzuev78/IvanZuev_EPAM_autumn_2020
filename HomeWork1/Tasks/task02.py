2dbcf6bd91e2c415ca29f14b03e49d5cbc8db00d
IvanZuev <ivan78.ru@yandex.ru>
2020-11-02 17:51:23 +0300

    Branch with tasks created.

--- HomeWork1/Tasks/task02.py
+++ HomeWork1/Tasks/task02.py
@@ -9,17 +9,5 @@ We guarantee, that the given sequence contain >= 0 integers inside.
 from collections import Sequence
 
 
-def check_fibonacci(data: Sequence) -> bool:
-
-    if len(data) == 0:
-        return False
-    for index, numb in enumerate(data):
-        if index > 1:
-            if data[index - 2] + data[index - 1] != numb:
-                return False
-        elif index == 0 and numb not in (0, 1):
-            return False
-        elif index == 1 and numb != 1:
-            return False
-
-    return True
+def check_fibonacci(data: Sequence[int]) -> bool:
+    ...
