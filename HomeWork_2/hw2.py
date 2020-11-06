8b371b28cbed2db3393cd682834c5a9c379a6ed5
Ivan <ivan78.ru@yandex.ru>
2020-11-06 16:20:29 +0300

    HomeWork_2 task_2 done

--- HomeWork_2/hw2.py
+++ HomeWork_2/hw2.py
@@ -17,8 +17,45 @@ Input: [2,2,1,1,1,2,2]
 Output: 2, 1
 
 """
+import unittest
 from typing import List, Tuple
 
 
 def major_and_minor_elem(inp: List) -> Tuple[int, int]:
-    ...
+
+    counter = {}
+
+    major_elem = None
+    minor_elem = None
+
+    for elem in inp:
+        if elem not in counter:
+            counter[elem] = 0
+        counter[elem] += 1
+
+    for elem in counter:
+        if major_elem is None:
+            major_elem = elem
+            minor_elem = elem
+        elif counter[elem] > counter[major_elem]:
+            major_elem = elem
+        elif counter[elem] < counter[minor_elem]:
+            minor_elem = elem
+
+    return major_elem, minor_elem
+
+
+class MyTestCase(unittest.TestCase):
+    def test_major_and_minor_elem(self):
+
+        input_1 = [2, 2, 1, 1, 1, 2, 2]
+        input_2 = [3, 2, 3]
+        input_3 = [i for i in range(20)] + [i for i in range(19)] + [10]
+
+        self.assertEqual(major_and_minor_elem(input_1), (2, 1))
+        self.assertEqual(major_and_minor_elem(input_2), (3, 2))
+        self.assertEqual(major_and_minor_elem(input_3), (10, 19))
+
+
+if __name__ == "__main__":
+    unittest.main()
