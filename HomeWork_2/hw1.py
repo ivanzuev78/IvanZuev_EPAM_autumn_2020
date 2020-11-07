b51aee8b4055aa1d14f6bb8096b0d4004e174371
IvanZuev <ivan78.ru@yandex.ru>
2020-11-07 19:11:12 +0300

    Homework 2 task 1 done

--- HomeWork_2/hw1.py
+++ HomeWork_2/hw1.py
@@ -6,24 +6,119 @@ Given a file containing text. Complete using only default collections:
     4) Count every non ascii char
     5) Find most common non ascii char for document
 """
+from collections import Counter
 from typing import List
 
 
+def reader(file_path: str, output: str) -> List:
+    """
+    output: ['WORDS', 'SYMBOLS']
+    :param file_path:
+    :param output:
+    :return:
+    """
+    word = []
+    parsed_text = []
+    symbols = []
+    skip_unicode_letter = 0
+    with open(file_path, "r") as file:
+
+        for line in file:
+            for index, letter in enumerate(line):
+
+                if skip_unicode_letter:
+                    skip_unicode_letter -= 1
+                    continue
+
+                elif letter == "\\":
+                    letter = line[index : index + 6]
+                    skip_unicode_letter += 5
+                    word.append(letter)
+                    symbols.append(letter)
+
+                elif letter.isalpha():
+                    word.append(letter)
+                    symbols.append(letter)
+
+                else:
+                    symbols.append(letter)
+                    if letter == "-" and line[index + 1] == "\n":
+                        break
+
+                    elif word:
+                        parsed_text.append(tuple(word))
+                        word = []
+    print(symbols)
+    if output == "WORDS":
+        return parsed_text
+    elif output == "SYMBOLS":
+        return symbols
+
+
 def get_longest_diverse_words(file_path: str) -> List[str]:
-    ...
+
+    text = reader(file_path, output="WORDS")
+    text = list(set(text))
+
+    text.sort(key=lambda word: len(frozenset(word)), reverse=True)
+
+    list_to_return = []
+
+    for word in text[:10]:
+        word_to_add = ""
+        for letter in word:
+            word_to_add += letter
+        list_to_return.append(word_to_add)
+
+    return list_to_return
 
 
 def get_rarest_char(file_path: str) -> str:
-    ...
+
+    symbols = reader(file_path, output="SYMBOLS")
+
+    counter = Counter()
+
+    for i in symbols:
+        counter[i] += 1
+
+    return counter.most_common()[-1][0]
 
 
 def count_punctuation_chars(file_path: str) -> int:
-    ...
+
+    symbols = reader(file_path, output="SYMBOLS")
+
+    counter = 0
+
+    for i in symbols:
+        if i in ".,:;-()!?":
+            counter += 1
+
+    return counter
 
 
 def count_non_ascii_chars(file_path: str) -> int:
-    ...
+
+    symbols = reader(file_path, output="SYMBOLS")
+
+    counter = 0
+
+    for i in symbols:
+        if len(i) > 1:
+            counter += 1
+
+    return counter
 
 
 def get_most_common_non_ascii_char(file_path: str) -> str:
-    ...
+
+    symbols = reader(file_path, output="SYMBOLS")
+
+    counter = Counter()
+
+    for i in symbols:
+        if len(i) > 1:
+            counter[i] += 1
+
+    return counter.most_common()[0][0]
---
+++ HomeWork_2/test_hw1.py
@@ -0,0 +1,64 @@
+import os
+from HomeWork_2.hw1 import *
+
+
+def create_txt_file(text: List[str], filename):
+
+    with open(filename, "w") as file:
+        for i in text:
+            file.write(i + "\n")
+
+
+def test_get_longest_diverse_words():
+
+    text_nununique_symbols = ["x" * i for i in range(1, 11)]
+
+    # Генерирую такой большой массив, потому что надо минимум 10 слов с уникальными символами
+    text_unique_symbols = ["".join(chr(97 + i) for i in range(k)) for k in range(2, 12)]
+
+    create_txt_file(text_nununique_symbols + text_unique_symbols, "test_text.txt")
+
+    assert set(get_longest_diverse_words("test_text.txt")) == set(text_unique_symbols)
+
+
+def test_get_rarest_char():
+
+    text = ["asd asd qwe-\nr qwe r p \n"]
+
+    create_txt_file(text, "test_text.txt")
+
+    assert get_rarest_char("test_text.txt") == "p"
+
+    os.remove("test_text.txt")
+
+
+def test_count_punctuation_chars():
+
+    text = ["I. believe: i, can fly!!!"]
+
+    create_txt_file(text, "test_text.txt")
+
+    assert count_punctuation_chars("test_text.txt") == 6
+
+    os.remove("test_text.txt")
+
+
+def test_count_non_ascii_chars():
+    text = ["\\u00bbJetzt und hier\\u00ab \\u00bb"]
+
+    create_txt_file(text, "test_text.txt")
+
+    assert count_non_ascii_chars("test_text.txt") == 3
+
+    os.remove("test_text.txt")
+
+
+def test_get_most_common_non_ascii_char():
+
+    text = ["\\u00bbJetzt und hier\\u00ab \\u00bb"]
+
+    create_txt_file(text, "test_text.txt")
+
+    assert get_most_common_non_ascii_char("test_text.txt") == "\\u00bb"
+
+    os.remove("test_text.txt")
