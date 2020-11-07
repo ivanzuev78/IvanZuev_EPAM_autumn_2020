3c8badf7f7ce7069003d18cd1a7bb058c5e70b56
IvanZuev <ivan78.ru@yandex.ru>
2020-11-07 22:46:47 +0300

    Homework 1 task 4 modified

--- HomeWork1/Tests/test_task04.py
+++ HomeWork1/Tests/test_task04.py
@@ -1,17 +1,21 @@
-import unittest
-from ..Tasks.task04 import check_sum_of_four
+from HomeWork1.Tasks.task04 import check_sum_of_four
 
 
-class MyTestCase(unittest.TestCase):
-    def test_check_sum_of_four(self):
+def test_check_sum_of_four():
 
-        input_1 = [-20, -30]
-        input_2 = [20, 30]
-        input_3 = [-100, -200]
-        input_4 = [100, 200]
+    input_1 = [-20, -30]
+    input_2 = [20, 30]
+    input_3 = [-100, -200]
+    input_4 = [100, 200]
 
-        self.assertEqual(check_sum_of_four(input_1, input_2, input_3, input_4), 4)
+    assert check_sum_of_four(input_1, input_2, input_3, input_4) == 4
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_check_sum_of_four_2():
+
+    input_1 = [20, 30]
+    input_2 = [20, 30]
+    input_3 = [100, 200]
+    input_4 = [100, 200]
+
+    assert check_sum_of_four(input_1, input_2, input_3, input_4) == 0
