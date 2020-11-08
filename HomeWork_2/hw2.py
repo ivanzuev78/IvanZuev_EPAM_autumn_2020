3d9c6cb4c1f8c433cc51cf466a1bfaa97630a670
IvanZuev <ivan78.ru@yandex.ru>
2020-11-08 16:36:29 +0300

    Homework 2 task 2 modified.

--- HomeWork_2/hw2.py
+++ HomeWork_2/hw2.py
@@ -17,45 +17,44 @@ Input: [2,2,1,1,1,2,2]
 Output: 2, 1
 
 """
-import unittest
+from collections import Counter
 from typing import List, Tuple
 
 
 def major_and_minor_elem(inp: List) -> Tuple[int, int]:
 
-    counter = {}
+    counter = Counter()
 
-    major_elem = None
+    # major_elem = None
     minor_elem = None
 
     for elem in inp:
-        if elem not in counter:
-            counter[elem] = 0
         counter[elem] += 1
 
-    for elem in counter:
-        if major_elem is None:
-            major_elem = elem
-            minor_elem = elem
-        elif counter[elem] > counter[major_elem]:
-            major_elem = elem
-        elif counter[elem] < counter[minor_elem]:
-            minor_elem = elem
+    major_elem = counter.most_common(1)[0][0]
+    minor_elem = counter.most_common()[-1][0]
+
+    # for elem in counter:
+    #     if major_elem is None:
+    #         major_elem = elem
+    #         minor_elem = elem
+    #     elif counter[elem] > counter[major_elem]:
+    #         major_elem = elem
+    #     elif counter[elem] < counter[minor_elem]:
+    #         minor_elem = elem
 
     return major_elem, minor_elem
 
 
-class MyTestCase(unittest.TestCase):
-    def test_major_and_minor_elem(self):
+def test_major_and_minor_elem_1():
 
-        input_1 = [2, 2, 1, 1, 1, 2, 2]
-        input_2 = [3, 2, 3]
-        input_3 = [i for i in range(20)] + [i for i in range(19)] + [10]
+    input_1 = [2, 2, 1, 1, 1, 2, 2]
 
-        self.assertEqual(major_and_minor_elem(input_1), (2, 1))
-        self.assertEqual(major_and_minor_elem(input_2), (3, 2))
-        self.assertEqual(major_and_minor_elem(input_3), (10, 19))
+    assert major_and_minor_elem(input_1) == (2, 1)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_major_and_minor_elem_2():
+
+    input_2 = [3, 2, 3]
+
+    assert major_and_minor_elem(input_2) == (3, 2)
