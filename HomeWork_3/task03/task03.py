510ebb610aae0274f0d5cf223ae167fdaea8120e
IvanZuev <ivan78.ru@yandex.ru>
2020-11-11 23:16:14 +0300

    Homework 3 task 3 modified

--- HomeWork_3/task03/task03.py
+++ HomeWork_3/task03/task03.py
@@ -1,5 +1,5 @@
 # I decided to write a code that generates data filtering object from a list of keyword parameters:
-from typing import Callable, List, Hashable
+from typing import Callable, List, Hashable, Optional, Any, Iterable
 
 
 class Filter:
@@ -11,7 +11,7 @@ class Filter:
     def __init__(self, *functions: Callable):
         self.functions = functions
 
-    def apply(self, data: List[dict]) -> List[dict]:
+    def apply(self, data: Iterable[Any]) -> Iterable[Any]:
         return [item for item in data if all(func(item) for func in self.functions)]
 
 
@@ -20,28 +20,25 @@ class Filter:
 # positive_even.apply(range(100)) should return only even numbers from 0 to 99
 
 
-def make_filter(**keywords: dict) -> Filter:
+def make_filter(**keywords: Any) -> Filter:
     """
     Generate filter object for specified keywords
     """
 
-    def keyword_filter_func(key_to_check: Hashable):
+    def keyword_filter_func(keyword_to_check: Hashable):
         def wrapper(input_dict: dict) -> bool:
-
-            if (
-                key_to_check in input_dict
-                and keywords[key_to_check] == input_dict[key_to_check]
-            ):
-                return True
-            else:
-                return False
+            return (
+                keywords[keyword_to_check] == input_dict[keyword_to_check]
+                if keyword_to_check in input_dict
+                else False
+            )
 
         return wrapper
 
     filter_funcs = []
 
-    for key in keywords:
-        filter_funcs.append(keyword_filter_func(key))
+    for keyword in keywords:
+        filter_funcs.append(keyword_filter_func(keyword))
 
     return Filter(*filter_funcs)
 
--- HomeWork_3/task03/test_task_3.py
+++ HomeWork_3/task03/test_task_3.py
@@ -1,15 +1,18 @@
+import pytest
+
 from HomeWork_3.task03.task03 import make_filter, Filter
 
 
-def test_Filter():
+def test_filter():
 
     assert Filter(
         lambda a: a % 2 == 0, lambda a: a > 0, lambda a: isinstance(a, int)
-    ).apply(range(-10, 10)) == [2, 4, 6, 8]
+    ).apply([-4, -3, 0, 1, 2, 3, 4]) == [2, 4]
 
 
-def test_specified_filter_example():
-    sample_data = [
+@pytest.fixture
+def sample_data():
+    input = [
         {
             "name": "Bill",
             "last_name": "Gilbert",
@@ -18,40 +21,39 @@ def test_specified_filter_example():
         },
         {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
     ]
-    assert (
-        make_filter(name="polly", type="bird", last_name="Gilbert").apply(sample_data)
-        == []
-    )
-    assert make_filter(name="polly", type="bird").apply(sample_data) == [
-        {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"}
-    ]
+    yield input
 
 
-def test_specified_filter__no_one_in_input_are_good():
-    sample_data = [
+def test_specified_filter_example(sample_data):
+
+    assert make_filter(name="polly", type="bird").apply(sample_data) == [sample_data[1]]
+
+
+@pytest.fixture
+def specified_filter_input():
+    input = [
         {
             "name": "Bill",
             "last_name": "Gilbert",
-        },
-        {"kind": "parrot", "type": "bird"},
-    ]
-    assert make_filter(name="Ivan", type="man").apply(sample_data) == []
-    assert make_filter(name="Bill", type="bird").apply(sample_data) == []
-
-
-def test_specified_filter_all_in_input_are_good():
-    sample_data = [
-        {
-            "name": "Bill",
-            "last_name": "Gilbert",
+            "occupation": "was here",
+            "type": "person",
         },
         {
             "name": "Bill",
             "last_name": "Paper",
         },
-        {
-            "name": "Bill",
-            "last_name": "Wodoomagic",
-        },
+        {"name": "Bill", "last_name": "Wodoomagic", "type": "person"},
     ]
-    assert make_filter(name="Bill").apply(sample_data) == sample_data
+    yield input
+
+
+def test_specified_filter__no_one_in_input_are_good(specified_filter_input):
+
+    assert make_filter(name="Bill", type="bird").apply(specified_filter_input) == []
+
+
+def test_specified_filter_all_in_input_are_good(specified_filter_input):
+
+    assert (
+        make_filter(name="Bill").apply(specified_filter_input) == specified_filter_input
+    )
