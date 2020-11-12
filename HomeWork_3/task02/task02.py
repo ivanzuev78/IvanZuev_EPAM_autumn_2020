3e283c3033ce8559888bc0e6d02c10eab9e37f01
Ivan <ivan78.ru@yandex.ru>
2020-11-12 16:47:36 +0300

    Homework 3 task 2 modified

--- HomeWork_3/task02/task02.py
+++ HomeWork_3/task02/task02.py
@@ -13,20 +13,39 @@ def slow_calculate(value: int) -> int:
     return sum(struct.unpack("<" + "B" * len(data), data))
 
 
-def fast_calculate(values: List[int]) -> int:
+def fast_calculate(values: List[int], numb_of_threads=10) -> int:
+
+    if numb_of_threads > len(values):
+        numb_of_threads = len(values)
 
     total_sum_list = []
 
-    def tread_calculate(args):
-        total_sum_list.append(slow_calculate(args))
+    def tread_calculate(*args):
+        for val in args:
+            total_sum_list.append(slow_calculate(val))
 
     tasks = {}
+    values_ranges = []
+    interval_of_values_range = int(len(values) / numb_of_threads) + 1
 
-    for i in range(len(values)):
-        tasks[i] = threading.Thread(target=tread_calculate, args=[values[i]])
+    for i in range(numb_of_threads):
+        if values[i * interval_of_values_range : (i + 1) * interval_of_values_range]:
+
+            values_ranges.append(
+                values[
+                    i * interval_of_values_range : (i + 1) * interval_of_values_range
+                ]
+            )
+        else:
+            break
+
+    numb_of_threads = len(values_ranges)
+
+    for i in range(numb_of_threads):
+        tasks[i] = threading.Thread(target=tread_calculate, args=values_ranges[i])
         tasks[i].start()
 
-    for i in range(len(values)):
+    for i in range(numb_of_threads):
         tasks[i].join()
 
-    return sum(total_sum_list)
+    return sum(i for i in total_sum_list)
--- HomeWork_3/task02/test_task_2.py
+++ HomeWork_3/task02/test_task_2.py
@@ -1,5 +1,3 @@
-import time
-
 from HomeWork_3.task02.task02 import slow_calculate, fast_calculate
 
 
@@ -12,3 +10,21 @@ def test_calculate_equal_result():
     sum_fast = fast_calculate([i for i in range(3)])
 
     assert sum_slow == sum_fast
+
+
+def test_calculate_numb_of_threds_less_than_numb_of_values():
+
+    sum_slow = 0
+    for i in range(13):
+        sum_slow += slow_calculate(i)
+
+    sum_fast = fast_calculate([i for i in range(13)], numb_of_threads=7)
+
+    assert sum_slow == sum_fast
+
+
+def test_calculate_lots_of_threads():
+
+    sum_fast = fast_calculate([i for i in range(10**4)], numb_of_threads=10**4)
+
+    assert True
