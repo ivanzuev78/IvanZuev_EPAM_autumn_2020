6ecef68078b31791c0810ba034c4d28753e1ee4c
IvanZuev <ivan78.ru@yandex.ru>
2020-11-13 22:45:44 +0300

    Homework 4 task 3 done

--- HomeWork_4/task_3_get_print_output.py
+++ HomeWork_4/task_3_get_print_output.py
@@ -3,12 +3,12 @@ Write a function that will receive a string and write it to stderr
 if line starts with "error" and to the stdout otherwise.
 
 
->>> my_precious_logger("error: file not found")
+# >>> my_precious_logger("error: file not found")
 # stderr
 'error: file not found'
 
 
->>> my_precious_logger("OK")
+# >>> my_precious_logger("OK")
 # stdout
 'OK'
 
@@ -23,7 +23,11 @@ You will learn:
 
 
 """
+import sys
 
 
 def my_precious_logger(text: str):
-    pass
+    if text[:5] == "error":
+        sys.stderr.write(text)
+    else:
+        sys.stdout.write(text)
