e6cabe06f27e51b134e788691326087d8e66e051
IvanZuev <ivan78.ru@yandex.ru>
2020-11-13 22:45:44 +0300

    Homework 4 task 3 done

--- HomeWork_4/HW_4_task_3.py
+++ HomeWork_4/HW_4_task_3.py
@@ -1,6 +1,15 @@
-import pytest
+from HomeWork_4.task_3_get_print_output import my_precious_logger
 
 
-def test_stderr():
-    assert captured.out == "hello\n"
-    assert captured.err == "world\n"
+def test_my_precious_logger_stderr(capsys):
+
+    my_precious_logger("error: file not found")
+    captured = capsys.readouterr()
+    assert captured.err == "error: file not found"
+
+
+def test_my_precious_logger_stdout(capsys):
+
+    my_precious_logger("May the Force be with you")
+    captured = capsys.readouterr()
+    assert captured.out == "May the Force be with you"
