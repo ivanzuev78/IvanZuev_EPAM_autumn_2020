9da8e4daf977a75f44719bda64bf8b8f3cb87a55
IvanZuev <ivan78.ru@yandex.ru>
2020-11-13 21:23:42 +0300

    Homework 3 task 1 modified

--- HomeWork_3/task01/test_task01.py
+++ HomeWork_3/task01/test_task01.py
@@ -12,9 +12,11 @@ def func(a, b):
 def test_cache_val1_is_val_2():
     some_args = 100, 200
 
-    val_1 = func(*some_args)
-    val_2 = func(*some_args)
-    val_3 = func(*some_args)
-    val_4 = func(*some_args)
+    first_result = func(*some_args)
+    cached_result = func(*some_args)
+    last_result_from_cache = func(*some_args)
+    non_cached_result = func(*some_args)
 
-    assert val_1 is val_2 is val_3 is not val_4
+    assert first_result is cached_result
+    assert cached_result is last_result_from_cache
+    assert last_result_from_cache is not non_cached_result
