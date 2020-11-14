b830ea51d9f3784cf0b83baf398bb52eb9341292
IvanZuev <ivan78.ru@yandex.ru>
2020-11-14 18:11:47 +0300

    Homework 4 task 3 modified

--- HomeWork_4/test_task_3.py
+++ HomeWork_4/test_task_3.py
@@ -1,25 +1,19 @@
-import pytest
-
 from HomeWork_4.task_3_get_print_output import my_precious_logger
 
 
 def test_my_precious_logger_stdout(capsys):
-
     my_precious_logger("May the Force be with you")
     captured = capsys.readouterr()
     assert captured.out == "May the Force be with you"
 
 
 def test_my_precious_logger_stderr(capsys):
-
     my_precious_logger("error: Force not found")
     captured = capsys.readouterr()
     assert captured.err == "error: Force not found"
 
 
-def test_my_precious_logger_wrong_input():
-
-    with pytest.raises(
-        TypeError, match="Input object must be  <class 'str'>, not <class 'int'>"
-    ):
-        my_precious_logger(2134567)
+def test_my_precious_logger_empty_input(capsys):
+    my_precious_logger("")
+    captured = capsys.readouterr()
+    assert captured.out == ""
