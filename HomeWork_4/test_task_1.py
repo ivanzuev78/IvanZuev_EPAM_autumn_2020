51e89c533757d8ee84d7205ced1f5b7fc1c7e005
IvanZuev <ivan78.ru@yandex.ru>
2020-11-15 16:13:28 +0300

    Homework 4 task 1 content creator created

--- HomeWork_4/test_task_1.py
+++ HomeWork_4/test_task_1.py
@@ -50,6 +50,19 @@ def input_int_out_of_range():
     os.remove(name_of_file)
 
 
+@pytest.fixture
+def content_creator():
+    name_of_file = "data_from_content_creator.txt"
+
+    def wrapper(text):
+        with open(name_of_file, "w") as file:
+            file.write(text)
+        return name_of_file
+
+    yield wrapper
+    os.remove(name_of_file)
+
+
 def test_read_magic_number_input_int_numb_1(input_int_1_true):
     assert read_magic_number(input_int_1_true) is True
 
@@ -77,3 +90,14 @@ def test_read_magic_number_file_doesnt_exists():
         ValueError, match='File "non-existing_file.txt" doesn\'t exists.'
     ):
         read_magic_number("non-existing_file.txt")
+
+
+def test_read_magic_number_with_content_creator_good_input(content_creator):
+    file_name = content_creator("1\nhello")
+    assert read_magic_number(file_name) is True
+
+
+def test_read_magic_number_with_content_creator_bad_input(content_creator):
+    file_name = content_creator("World\nhello")
+    with pytest.raises(ValueError, match="First line must be integer!"):
+        read_magic_number(file_name)
