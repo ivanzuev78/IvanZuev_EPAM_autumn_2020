fff21269123834acc8083f09f7615b6384f1aa02
IvanZuev <ivan78.ru@yandex.ru>
2020-11-15 10:55:57 +0300

    Homework 4 task 5 started

--- HomeWork_4/task_5_optional.py
+++ HomeWork_4/task_5_optional.py
@@ -12,14 +12,46 @@ Definition of done:
  - function has tests
 
 
->>> list(fizzbuzz(5))
-["1", "2", "fizz", "4", "buzz"]
+# >>> list(fizzbuzz(5))
+# ["1", "2", "fizz", "4", "buzz"]
 
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** https://www.youtube.com/watch?v=NSzsYWckGd4
 """
+from itertools import cycle
 from typing import List, Generator
 
 
-def fizzbuzz(n: int) -> Generator[str]:
-    pass
+def return_next(value):
+    while True:
+        a = cycle(
+            [
+                f"{value}",
+                f"{i}",
+                "fizz",
+                f"{i}",
+                "buzz",
+                "fizz",
+                f"{i}",
+                f"{i}",
+                f"{i}",
+                "buzz",
+                f"{i}",
+                "fizz",
+                f"{i}",
+                f"{i}",
+                "fizzbuzz",
+            ]
+        )
+        value = yield next(a)
+
+
+def fizzbuzz(n: int):
+
+    for i in range(1, n + 1):
+        yield next(a)
+
+
+gen = fizzbuzz(32)
+for i in range(1, 32):
+    print(i, next(gen))
