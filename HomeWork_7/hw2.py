4911f8bc8111cc3f89d9124713da46ca46e9f174
IvanZuev <ivan78.ru@yandex.ru>
2020-12-02 22:31:45 +0300

    Homework 7 task 2 done

--- HomeWork_7/hw2.py
+++ HomeWork_7/hw2.py
@@ -1,24 +1,33 @@
-"""
-Given two strings. Return if they are equal when both are typed into
-empty text editors. # means a backspace character.
+def backspace_compare(first: str, second: str) -> bool:
+    """
+    Given two strings. Return if they are equal when both are typed into
+    empty text editors. # means a backspace character.
 
-Note that after backspacing an empty text, the text will continue empty.
+    Note that after backspacing an empty text, the text will continue empty.
 
-Examples:
-    Input: s = "ab#c", t = "ad#c"
-    Output: True
-    # Both s and t become "ac".
+    Examples:
+        Input: s = "ab#c", t = "ad#c"
+        Output: True
+        # Both s and t become "ac".
 
-    Input: s = "a##c", t = "#a#c"
-    Output: True
-    Explanation: Both s and t become "c".
+        Input: s = "a##c", t = "#a#c"
+        Output: True
+        Explanation: Both s and t become "c".
 
-    Input: a = "a#c", t = "b"
-    Output: False
-    Explanation: s becomes "c" while t becomes "b".
+        Input: a = "a#c", t = "b"
+        Output: False
+        Explanation: s becomes "c" while t becomes "b".
 
-"""
+    """
 
+    def type_this_string(string_to_type):
+        real_typing_string = ""
+        for char in string_to_type:
+            if char == "#":
+                real_typing_string = real_typing_string[:-1]
+            else:
+                real_typing_string += char
+            print(real_typing_string)
+        return real_typing_string
 
-def backspace_compare(first: str, second: str):
-    ...
+    return type_this_string(first) == type_this_string(second)
