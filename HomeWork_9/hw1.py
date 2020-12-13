ed933fbdc60c0e1119d190df07854569f62dae54
IvanZuev <ivan78.ru@yandex.ru>
2020-12-13 21:04:23 +0300

    Homework 9 task 1 done

--- HomeWork_9/hw1.py
+++ HomeWork_9/hw1.py
@@ -11,12 +11,26 @@ file2.txt:
 4
 6
 
->>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
+# >>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
 [1, 2, 3, 4, 5, 6]
 """
 from pathlib import Path
 from typing import Iterator, List, Union
 
 
-def merge_sorted_files(file_list: List[Union[Path, str], ...]) -> Iterator:
-    pass
+def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
+    opened_files = []
+    for file in file_list:
+        with open(file, "r") as f:
+            opened_files.append(list(f))
+
+    while opened_files:
+        current_list_to_take_numb = None
+        for index, numb_list in enumerate(opened_files):
+            if current_list_to_take_numb is None:
+                current_list_to_take_numb = index
+            elif int(numb_list[0]) < int(opened_files[current_list_to_take_numb][0]):
+                current_list_to_take_numb = index
+        yield int(opened_files[current_list_to_take_numb].pop(0))
+        if not opened_files[current_list_to_take_numb]:
+            opened_files.pop(current_list_to_take_numb)
