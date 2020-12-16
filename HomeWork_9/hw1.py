594809213f3c82e1514b329adbdfa8bf5c2d873e
IvanZuev <ivan78.ru@yandex.ru>
2020-12-16 21:02:14 +0300

    HW 6 task 2 oop2 modified

--- HomeWork_9/hw1.py
+++ HomeWork_9/hw1.py
@@ -22,15 +22,18 @@ def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
     opened_files = []
     for file in file_list:
         with open(file, "r") as f:
-            opened_files.append(list(f))
+            opened_files.append(list(reversed(list(f))))
 
     while opened_files:
         current_list_to_take_numb = None
+        current_list_index = None
         for index, numb_list in enumerate(opened_files):
             if current_list_to_take_numb is None:
-                current_list_to_take_numb = index
-            elif int(numb_list[0]) < int(opened_files[current_list_to_take_numb][0]):
-                current_list_to_take_numb = index
-        yield int(opened_files[current_list_to_take_numb].pop(0))
-        if not opened_files[current_list_to_take_numb]:
-            opened_files.pop(current_list_to_take_numb)
+                current_list_to_take_numb = opened_files[0]
+                current_list_index = index
+            elif int(numb_list[-1]) < int(current_list_to_take_numb[-1]):
+                current_list_to_take_numb = numb_list
+                current_list_index = index
+        yield int(current_list_to_take_numb.pop(-1))
+        if not current_list_to_take_numb:
+            opened_files.pop(current_list_index)
