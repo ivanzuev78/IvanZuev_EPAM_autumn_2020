f94a6bafc410eaba5d8b7f9f8dc539b40769b5d9
IvanZuev <ivan78.ru@yandex.ru>
2020-12-19 23:30:03 +0300

    HW 9 task 1 fixed

--- HomeWork_9/hw1.py
+++ HomeWork_9/hw1.py
@@ -19,22 +19,16 @@ from typing import Iterator, List, Union
 
 
 def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
-    def read_next_numb_from_file(file_manager_dict: dict) -> dict:
-        with open(file_manager_dict["filename"], "r") as f:
-            f.seek(file_manager_dict["cursor_position"])
-            numb = f.readline()
-            if numb[:-1].isdigit():
-                file_manager_dict["numb"] = int(numb)
-            else:
-                file_manager_dict["numb"] = None
-            file_manager_dict["cursor_position"] = f.tell()
-            return file_manager_dict
 
     opened_files = {}
-    for file in file_list:
-        opened_files[file] = read_next_numb_from_file(
-            {"filename": file, "cursor_position": 0, "numb": None}
-        )
+    for filename in file_list:
+        file = open(filename, "r")
+        opened_files[filename] = {"file": file}
+        numb = next(opened_files[filename]["file"])
+        if numb[:-1].isdigit():
+            opened_files[filename]["numb"] = int(numb[:-1])
+        else:
+            raise ValueError("String is not a numb")
 
     while opened_files:
         current_file_manager_dict_to_take_numb = None
@@ -47,8 +41,13 @@ def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
             ):
                 current_file_manager_dict_to_take_numb = filename
         yield opened_files[current_file_manager_dict_to_take_numb]["numb"]
-        opened_files[current_file_manager_dict_to_take_numb] = read_next_numb_from_file(
-            opened_files[current_file_manager_dict_to_take_numb]
-        )
-        if opened_files[current_file_manager_dict_to_take_numb]["numb"] is None:
+        try:
+            numb = next(opened_files[current_file_manager_dict_to_take_numb]["file"])
+            opened_files[current_file_manager_dict_to_take_numb]["numb"] = int(
+                numb[:-1]
+            )
+        except StopIteration:
+            opened_files[current_file_manager_dict_to_take_numb]["file"].close()
             del opened_files[current_file_manager_dict_to_take_numb]
+        except ValueError:
+            raise ValueError("String is not a numb")
