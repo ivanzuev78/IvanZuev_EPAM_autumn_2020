734ced06b7f0e038a0f35aec87b618aa5ac95df9
IvanZuev <ivan78.ru@yandex.ru>
2020-12-23 22:54:45 +0300

    HW 8 task 2 modified

--- HomeWork_8/test_task_2.py
+++ HomeWork_8/test_task_2.py
@@ -30,20 +30,20 @@ def test_tabledata_with_good_database_name_and_column(database_name):
     assert data["Name_not_DB"] is None
 
 
-def test_tabledata_len_protocol(database_name):
-    data = TableData(database_name, "test")
-    assert len(data) == 2
+def test_tabledata_len_protocol():
+    data = TableData("example.sqlite", "presidents")
+    assert len(data) == 3
 
 
-def test_tabledata_iterable_protocol(database_name):
-    data = TableData(database_name, "test")
-    names = ["Yeltsin", "Trump"]
+def test_tabledata_iterable_protocol():
+    data = TableData("example.sqlite", "presidents")
+    names = ["Yeltsin", "Trump", "Big Man Tyrone"]
     for index, man in enumerate(data):
         assert man["name"] == names[index]
 
 
-def test_tabledata_contains_protocol(database_name):
-    data = TableData(database_name, "test")
+def test_tabledata_contains_protocol():
+    data = TableData("example.sqlite", "presidents")
     assert ("Trump" in data) is True
     assert ("Bill" in data) is False
 
@@ -53,13 +53,14 @@ def test_tabledata_returns_recent_data(database_name):
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO test(name, param_str) VALUES(?, ?)", ("man1", "test_str")
+        "INSERT INTO presidents(name, age, country) VALUES(?, ?, ?)",
+        ("man1", "4567", "Lapland"),
     )
     conn.commit()
     assert ("man1" in data) is True
     conn.close()
 
 
-def test_tabledata_get_wrong_column_name(database_name):
+def test_tabledata_get_wrong_column_name():
     with pytest.raises(AttributeError):
-        TableData(database_name, "wrong column")
+        TableData("example.sqlite", "wrong column")
