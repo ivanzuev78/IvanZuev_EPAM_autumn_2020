90fc111f529d35546274f746e3ad8bd6d26cbd59
IvanZuev <ivan78.ru@yandex.ru>
2020-12-23 22:49:46 +0300

    HW 8 task 2 modified

--- HomeWork_8/test_task_2.py
+++ HomeWork_8/test_task_2.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 import sqlite3
 
 from HomeWork_8.task_2 import TableData
@@ -7,50 +5,45 @@ from HomeWork_8.task_2 import TableData
 import pytest
 
 
-@pytest.fixture()
-def copied_db_to_test_added_data():
-    shutil.copyfile("example_new.sqlite", "example_2.sqlite")
-    yield "example_2.sqlite"
-    os.remove("example_2.sqlite")
-
-
 def test_tabledata_with_good_database_name_and_column():
-    data = TableData("example_new.sqlite", "presidents")
+    data = TableData("example.sqlite", "presidents")
     assert data["Yeltsin"] == {"age": 999, "country": "Russia", "name": "Yeltsin"}
     assert data["Name_not_DB"] is None
 
 
 def test_tabledata_len_protocol():
-    data = TableData("example_new.sqlite", "presidents")
+    data = TableData("example.sqlite", "presidents")
     assert len(data) == 3
 
 
 def test_tabledata_iterable_protocol():
-    data = TableData("example_new.sqlite", "presidents")
+    data = TableData("example.sqlite", "presidents")
     names = ["Yeltsin", "Trump", "Big Man Tyrone"]
     for index, man in enumerate(data):
         assert man["name"] == names[index]
 
 
 def test_tabledata_contains_protocol():
-    data = TableData("example_new.sqlite", "presidents")
+    data = TableData("example.sqlite", "presidents")
     assert ("Trump" in data) is True
     assert ("Bill" in data) is False
 
 
-def test_tabledata_returns_recent_data(copied_db_to_test_added_data):
-    data = TableData(copied_db_to_test_added_data, "presidents")
-    conn = sqlite3.connect(copied_db_to_test_added_data)
+def test_tabledata_returns_recent_data():
+    data = TableData("example.sqlite", "presidents")
+    conn = sqlite3.connect("example.sqlite")
     cursor = conn.cursor()
     cursor.execute(
         "INSERT INTO presidents(name, age, country) VALUES(?, ?, ?)",
         ("man1", "4567", "Lapland"),
     )
     conn.commit()
-    conn.close()
     assert ("man1" in data) is True
+    cursor.execute("DELETE FROM presidents WHERE name = ?", ("man1",))
+    conn.commit()
+    conn.close()
 
 
 def test_tabledata_get_wrong_column_name():
     with pytest.raises(AttributeError):
-        TableData("example_new.sqlite", "wrong column")
+        TableData("example.sqlite", "wrong column")
