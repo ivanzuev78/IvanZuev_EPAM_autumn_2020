a9b8564f89f37e245771b2a33fd1e2647467897e
IvanZuev <ivan78.ru@yandex.ru>
2020-12-23 22:31:15 +0300

    HW 8 task 2 modified

--- HomeWork_8/test_task_2.py
+++ HomeWork_8/test_task_2.py
@@ -9,31 +9,31 @@ import pytest
 
 @pytest.fixture()
 def copied_db_to_test_added_data():
-    shutil.copyfile("example.sqlite", "example_2.sqlite")
+    shutil.copyfile("example_new.sqlite", "example_2.sqlite")
     yield "example_2.sqlite"
     os.remove("example_2.sqlite")
 
 
 def test_tabledata_with_good_database_name_and_column():
-    data = TableData("example.sqlite", "presidents")
+    data = TableData("example_new.sqlite", "presidents")
     assert data["Yeltsin"] == {"age": 999, "country": "Russia", "name": "Yeltsin"}
     assert data["Name_not_DB"] is None
 
 
 def test_tabledata_len_protocol():
-    data = TableData("example.sqlite", "presidents")
+    data = TableData("example_new.sqlite", "presidents")
     assert len(data) == 3
 
 
 def test_tabledata_iterable_protocol():
-    data = TableData("example.sqlite", "presidents")
+    data = TableData("example_new.sqlite", "presidents")
     names = ["Yeltsin", "Trump", "Big Man Tyrone"]
     for index, man in enumerate(data):
         assert man["name"] == names[index]
 
 
 def test_tabledata_contains_protocol():
-    data = TableData("example.sqlite", "presidents")
+    data = TableData("example_new.sqlite", "presidents")
     assert ("Trump" in data) is True
     assert ("Bill" in data) is False
 
@@ -53,4 +53,4 @@ def test_tabledata_returns_recent_data(copied_db_to_test_added_data):
 
 def test_tabledata_get_wrong_column_name():
     with pytest.raises(AttributeError):
-        TableData("example.sqlite", "wrong column")
+        TableData("example_new.sqlite", "wrong column")
