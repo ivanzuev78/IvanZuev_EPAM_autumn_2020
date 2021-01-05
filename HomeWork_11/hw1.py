8cc67ee1b17fc8e25943c0e4b7b02fa9e03a2cfb
Ivan <ivan78.ru@yandex.ru>
2021-01-05 22:23:24 +0300

    HW 11 task 1 done

--- HomeWork_11/hw1.py
+++ HomeWork_11/hw1.py
@@ -1,42 +1,51 @@
-"""
-Vasya implemented nonoptimal Enum classes.
-Remove duplications in variables declarations using metaclasses.
-
-from enum import Enum
-
-
-class ColorsEnum(Enum):
-    RED = "RED"
-    BLUE = "BLUE"
-    ORANGE = "ORANGE"
-    BLACK = "BLACK"
-
-
-class SizesEnum(Enum):
-    XL = "XL"
-    L = "L"
-    M = "M"
-    S = "S"
-    XS = "XS"
-
-
-Should become:
-
-class ColorsEnum(metaclass=SimplifiedEnum):
-    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
-
-
-class SizesEnum(metaclass=SimplifiedEnum):
-    __keys = ("XL", "L", "M", "S", "XS")
-
-
-assert ColorsEnum.RED == "RED"
-assert SizesEnum.XL == "XL"
-"""
-
-
 class SimplifiedEnum(type):
-    def __getattr__(self, item):
-        if f'_{self.__name__}__keys' in self.__dict__:
-            if item in self.__dict__[f'_{self.__name__}__keys']:
+    """
+    Vasya implemented nonoptimal Enum classes.
+    Remove duplications in variables declarations using metaclasses.
+
+    from enum import Enum
+
+
+    class ColorsEnum(Enum):
+        RED = "RED"
+        BLUE = "BLUE"
+        ORANGE = "ORANGE"
+        BLACK = "BLACK"
+
+
+    class SizesEnum(Enum):
+        XL = "XL"
+        L = "L"
+        M = "M"
+        S = "S"
+        XS = "XS"
+
+
+    Should become:
+
+    class ColorsEnum(metaclass=SimplifiedEnum):
+        __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+    class SizesEnum(metaclass=SimplifiedEnum):
+        __keys = ("XL", "L", "M", "S", "XS")
+
+
+    assert ColorsEnum.RED == "RED"
+    assert SizesEnum.XL == "XL"
+    """
+
+    def __getattr__(cls, item):
+        if f"_{cls.__name__}__keys" in cls.__dict__:
+            if item in cls.__dict__[f"_{cls.__name__}__keys"]:
                 return str(item)
+
+    def __iter__(cls):
+        if f"_{cls.__name__}__keys" in cls.__dict__:
+            return (item for item in cls.__dict__[f"_{cls.__name__}__keys"])
+        return ()
+
+    def __len__(cls):
+        if f"_{cls.__name__}__keys" in cls.__dict__:
+            return len(cls.__dict__[f"_{cls.__name__}__keys"])
+        return 0
--- HomeWork_11/test_task_1.py
+++ HomeWork_11/test_task_1.py
@@ -1,13 +1,19 @@
+from collections.abc import Iterable
+
 from HomeWork_11.hw1 import SimplifiedEnum
 
 
-def test_simplified_enum_colors():
-    class ColorsEnum(metaclass=SimplifiedEnum):
-        __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+def test_simplified_enum():
     assert ColorsEnum.RED == "RED"
 
 
-def test_simplified_enum_sizes():
-    class SizesEnum(metaclass=SimplifiedEnum):
-        __keys = ("XL", "L", "M", "S", "XS")
-    assert SizesEnum.XL == "XL"
+def test_simplified_enum_len():
+    assert len(ColorsEnum) == 4
+
+
+def test_simplified_enum_is_iterable():
+    assert isinstance(ColorsEnum, Iterable)
