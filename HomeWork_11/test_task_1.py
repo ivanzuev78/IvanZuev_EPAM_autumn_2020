99cfb5bca7da21548ed7f6776e726699ba10043d
Ivan <ivan78.ru@yandex.ru>
2021-01-10 21:31:49 +0300

    HW_11 task 1 test added

--- HomeWork_11/test_task_1.py
+++ HomeWork_11/test_task_1.py
@@ -1,5 +1,7 @@
 from HomeWork_11.hw1 import SimplifiedEnum
 
+import pytest
+
 
 class ColorsEnum(metaclass=SimplifiedEnum):
     __keys = ("RED", "BLUE", "ORANGE", "BLACK")
@@ -10,3 +12,8 @@ def test_simplified_enum():
     assert ColorsEnum.BLUE == "BLUE"
     assert ColorsEnum.ORANGE == "ORANGE"
     assert ColorsEnum.BLACK == "BLACK"
+
+
+def test_simplified_enum_doesnt_have_other_atrs():
+    with pytest.raises(AttributeError):
+        ColorsEnum.YELLOW
