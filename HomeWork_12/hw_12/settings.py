23034258b012269a91bd0e04ac266552aa735418
Ivan <ivan78.ru@yandex.ru>
2021-01-15 23:10:11 +0300

    HomeWork 12 Done

---
+++ HomeWork_12/hw_12/settings.py
@@ -0,0 +1,17 @@
+SECRET_KEY = "k63(v-dqf1p6dgbp1(4m!#yw^4=rq)35_xmfkraj29=ivs@gl*"
+
+
+INSTALLED_APPS = [
+    "hw_12_task_1",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "main.db",
+    }
+}
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
